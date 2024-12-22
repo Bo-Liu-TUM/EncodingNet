6b34c42a4532e6244e45208f618fde806eb294ca
liubo-SEU <bo.liu@tum.de>
2024-12-22 20:28:11 +0800

    Update model_tools.py

--- models/model_tools.py
+++ models/model_tools.py
@@ -84,7 +84,7 @@ def validate(val_loader, model, criterion, args, verbose=True, end_batch=None):
     with torch.no_grad():
         end = time.time()
         for i, (images, target) in enumerate(val_loader):
-            if args.gpu is not None:
+            if torch.cuda.is_available():
                 images = images.cuda(args.gpu, non_blocking=True)
                 target = target.cuda(args.gpu, non_blocking=True)
 
@@ -143,7 +143,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args, writer=None, e
         # measure data loading time
         data_time.update(time.time() - end)
 
-        if args.gpu is not None:
+        if torch.cuda.is_available():
             images = images.cuda(args.gpu, non_blocking=True)
             target = target.cuda(args.gpu, non_blocking=True)
 
